8ab77a53667a9d9356c5de272552d85120898f52
Christopher Collier <czcollier@gmail.com>
2025-07-10 14:32:18 -0700

    renamed directories to matched planned project layout

---
+++ local_dev/boot.py
@@ -0,0 +1,2 @@
+import sys
+sys.path = sys.path + ['', '/usr/lib/python311.zip', '/usr/lib/python3.11', '/usr/lib/python3.11/lib-dynload', '/home/czcollier/devel/czc/projects/pico/python/anemometer/test_harness/.venv/lib/python3.11/site-packages']
